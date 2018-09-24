742093f6de468e85eaea7444bc629540b09bbdfa
Vedansh <vedanshpachuri@gmail.com>
2018-09-24 22:02:26 +0530

    first commit

---
+++ src/main/java/com/example/jenkinscf/JenkinsCfApplication.java
@@ -0,0 +1,12 @@
+package com.example.jenkinscf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCfApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/jenkinscf/JenkinsCfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.jenkinscf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsCfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
